4f354ca8fd8e55eda8794fb3813219753cb01bb1
Samx134 <sam3663mas@proton.me>
2025-03-06 16:22:28 +0900

    Update test_kadai.py

--- test_kadai.py
+++ test_kadai.py
@@ -61,7 +61,7 @@ class TestHotelPlanisphere(object):
             page.screenshot(path="kadai_test1.png")
 
         #  確認
-            assert False
+            assert page.text_content("#date ~ div") == "翌日以降の日付を入力してください。", "日付エラーが出る事"
 #  ②名前が空の状態では予約できないこと。
 
     def test_noname(self):
